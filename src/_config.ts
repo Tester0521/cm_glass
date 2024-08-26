686827098e380a210bc1e93276644a2e64196838
me <qxxd@0x00.local>
2024-08-26 17:31:41 +0300

    lol

---
+++ src/_config.ts
@@ -0,0 +1,14 @@
+import lume from "lume/mod.ts"
+import sass from "lume/plugins/sass.ts"
+
+const site = lume({
+  src: "./_src",
+  dest: "./_site",
+  prettyUrls: false,
+})
+
+
+site.copy("assets", ".")
+site.use(sass())
+
+export default site
